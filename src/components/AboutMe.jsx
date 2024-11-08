26fc7a0cf3cd020ece740950e8de6722f7a06885
AlexMartinez <173128885+AlaxMart@users.noreply.github.com>
2024-11-07 19:44:31 -0600

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -12,7 +12,7 @@ const AboutMe = () => {
                 ¡Hola! Soy Alejandro, un apasionado desarrollador web back-end, y aunque esta es mi especialidad, también tengo bases muy sólidas en el área del front-end. Mi flexibilidad me permite adaptarme a cualquier cambio. Soy determinado y puntual, cumpliendo rigurosamente con los plazos establecidos. Además, soy una persona que muy difícilmente se da por vencido, ya que para mí cada reto es una oportunidad para crecer y aprender. Con mi actitud proactiva, estoy listo para enfrentar y superar cualquier desafío.
             </p>
             <button className='about-me_btn'>
-                <a href="../../public/img/Cv.png" download='CV Alejandro Martinez'>Descargar CV</a>
+                <a href="/img/Cv.png" download='CV Alejandro Martinez'>Descargar CV</a>
             </button>
         </section>
     </div>
